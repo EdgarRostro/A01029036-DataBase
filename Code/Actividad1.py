7653032cd474bacbf9d095fb2c1847804a1ba44a
Edgar Rostro <Edgar Rostro>
2020-09-25 14:07:53 -0500

    Se arreglo codigo perdido

--- Code/Actividad1.py
+++ Code/Actividad1.py
@@ -1,11 +1,7 @@
-import csv
-
-with open("TextFiles/populationbycountry19802010millions.csv") as csvFile:
-    reader = csv.reader(csvFile)
-
-    for row in reader:
-         print(row[-1])
-
-    #reader = csv.DictReader(csvFile)
-
-    #print(reader.fieldnames)
+import pandas as pd
+filepath = "./TextFiles/populationbycountry19802010millions.csv"
+with open(filepath,'r') as fp:
+    data = fp.read().split('\n')
+    
+data = [f.split(',') for f in data]
+print (data[0][0])git 
